b4d593a2eab66fe9b427b0f68a9993312981defb
zino <jinho.bang@samsung.com>
2016-02-23 11:32:55 +0900

    Remove arrow functions.

--- service-worker/windowclient-navigate/service-worker.js
+++ service-worker/windowclient-navigate/service-worker.js
@@ -1,9 +1,9 @@
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim().then(() => {
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim().then(function() {
     // See https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
     return self.clients.matchAll({type: 'window'});
-  }).then(clients => {
-    return clients.map(client => {
+  }).then(function(clients) {
+    return clients.map(function(client) {
       // Check to make sure WindowClient.navigate() is supported.
       if ('navigate' in client) {
         return client.navigate('activated.html');
